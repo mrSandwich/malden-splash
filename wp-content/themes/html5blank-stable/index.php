8b5403f182032055e7542e1aa4eaf3872bdfcfdd
scojo <info@alisablanter.comm>
2017-01-30 10:24:12 -0500

    fonts, images, etc

---
+++ wp-content/themes/html5blank-stable/index.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+	<main role="main">
+		<!-- section -->
+		<section>
+
+			<!-- <h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1> -->
+
+
+		</section>
+		<!-- /section -->
+	</main>
+
+
+<?php get_footer(); ?>
