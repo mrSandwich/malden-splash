029fa20bc326a403f1396e477076b36534cfba1f
scojo <info@alisablanter.comm>
2017-02-07 15:34:09 -0500

    refactor js a bit

--- wp-content/themes/html5blank-stable/src/js/app.js
+++ wp-content/themes/html5blank-stable/src/js/app.js
@@ -19,11 +19,13 @@
   				arrows: false
 			});
 
-			this.initClickHandlers();
-			this.revealHiddenField();
+			this.initEventHandlers();
+			// this.revealHiddenField();
 
 		},
-		initClickHandlers: function() {
+		initEventHandlers: function() {
+			var _this 	 = this,
+				userForm = document.getElementById('user-form');
 
 			$('.cta, .main-cta a').on('click', function(e) {
 				var dest = $(".form-area").offset().top;
@@ -32,31 +34,31 @@
 				$("html:not(:animated),body:not(:animated)").animate({ scrollTop: dest}, 500 );
 			});
 
+			$(userForm).on('change', function() {
+
+				_this.revealHiddenField();
+
+			});
+
+
 		}, 
 		revealHiddenField: function() {
 
-			var form = document.getElementById("user-form"),
-				select = form.querySelectorAll('select')[0],
+			var select = document.querySelectorAll('select')[0],
 				hidden = document.getElementsByClassName('hidden-field')[0];
 
-			$(form).on('change', function() {
+			
 				if (select.value === "social media" || select.value === "referral" || select.value === "other") {
 					hidden.style.display = 'block';
 				} else {
 					hidden.style.display = 'none';
 				}
-			});
+
 		}
 	}
 
 	JAG.init();
 
-	$(function () {
-	
-	'use strict';
-	
-	// DOM ready, take it away
-		
-	});
+
 	
 })(jQuery, this);
